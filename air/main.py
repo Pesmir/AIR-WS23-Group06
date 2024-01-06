cebb897df5d875c3b56a05095cca0eb39e0c7044
Johannes Pesenhofer <johannes.pesenhofer@student.tugraz.at>
2024-01-06 23:16:44 +0100

    moved recommender call and fixed typo

--- air/main.py
+++ air/main.py
@@ -22,6 +22,30 @@ from air.processing.model_predictions import (
 from air.data.load import load_raw_data
 
 
+def call_recommender_system(data: pl.DataFrame):
+    selected_user = "A1CGLIDN7E5MK8"
+    recommender = RecommenderSystem(
+        data,
+        num_sim_users=5,  # lower means less conservative
+        num_recommendations=8,
+        rating_model="xgboost",
+        helpfulness_model="xgboost_helpfulness",
+    )
+    recs = recommender.recommend_books_based_on_similar_users(selected_user)
+    liked = (
+        data.filter(pl.col("User_id") == selected_user)
+        .sort(by="review/score")
+        .select("Title", "review/score")
+        .filter(pl.col("review/score") == 5.0)
+        .sample(n=8)
+    )
+
+    print("RECOMMENDED")
+    print(recs)
+    print("5 STAR BOOKS BY USER (Random 8 samples)")
+    print(liked)
+
+
 def main():
     data = load_raw_data()
     # 1. Preprocessing
@@ -34,7 +58,7 @@ def main():
     res = SplitReviewTokens().process(res)
     res = HelpfulnessPredictionInputProcessor().process(res)
     res = PreprocessingFinalizer().process(res)
-    # The models where trainend beforehand and the corresponding files are
+    # The models were trainend beforehand and the corresponding files are
     # loaded in air/processing/model_predictions.py
 
     # 2. Add Model predictions
@@ -47,27 +71,7 @@ def main():
     res = XGBoostHelpfulnessPredictionProcessor().process(res)
 
     # 3. Recommend book by user
-    selected_user = "A1CGLIDN7E5MK8"
-    recommender = RecommenderSystem(
-        res,
-        num_sim_users=5,  # lower means less conservative
-        num_recommendations=8,
-        rating_model="xgboost",
-        helpfulness_model="xgboost_helpfulness",
-    )
-    recs = recommender.recommend_books_based_on_similar_users(selected_user)
-    liked = (
-        res.filter(pl.col("User_id") == selected_user)
-        .sort(by="review/score")
-        .select("Title", "review/score")
-        .filter(pl.col("review/score") == 5.0)
-        .sample(n=8)
-    )
-
-    print("RECOMMENDED")
-    print(recs)
-    print("5 STAR BOOKS BY USER (Random 8 samples)")
-    print(liked)
+    call_recommender_system(res)
 
 
 if __name__ == "__main__":
