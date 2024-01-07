5efe51581531dd85d05a52935adb6947e6b96df6
JHE <julia.herold@bytepoets.com>
2024-01-07 08:00:39 +0100

    Refactor word2vec

--- air/notebooks/word2vec_training.py
+++ air/notebooks/word2vec_training.py
@@ -1,23 +1,20 @@
-# import logging
+import logging
 
 import numpy as np
 from gensim.models import Word2Vec
 import nltk
-from nltk.tokenize import word_tokenize
 
-from air.data.load import load_preprocessed_data_small
+from air.data.load import load_preprocessed_data
 
 nltk.download('punkt')
 
 # Use this to log information for build_vocab and train
-# logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
+logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
 
-input_df = load_preprocessed_data_small()
-# Drop tables with empty review text
-input_df = input_df.filter(input_df['preprocessed_review/text'].is_not_null())
-sentences = input_df['preprocessed_review/text'].to_list()
-# Tokenize TODO: add to preprocessing
-tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
+input_df = load_preprocessed_data()
+print(input_df.columns)
+sentences = input_df['preprocessed_review/tokens'].to_list()
+print(sentences[0])
 
 # Create model
 model = Word2Vec(min_count=20,
@@ -28,22 +25,37 @@ model = Word2Vec(min_count=20,
                  negative=20,
                  workers=4)
 # Add words
-model.build_vocab(corpus_iterable=tokenized_sentences, progress_per=100)
+model.build_vocab(corpus_iterable=sentences, progress_per=100)
 model.train(sentences, total_examples=model.corpus_count, epochs=10, report_delay=1.0)
-
-max_vector_length = max(len(model.wv[word]) for sentence in tokenized_sentences for word in sentence if word in model.wv.index_to_key)
-
-# Filter out sentences with no words in the vocabulary
-valid_sentences = [sentence for sentence in tokenized_sentences if any(word in model.wv.index_to_key for word in sentence)]
+print("Training done")
 
 # Create document vectors by averaging the word vectors
 document_vectors = []
-for sentence in valid_sentences:
-    word_vectors = [model.wv[word] for word in sentence if word in model.wv.index_to_key]
+count = 0
+max_vector_length = 0
+
+for sentence in sentences:
+    count += 1
+    if count % 100 == 0:
+        print(count)
+
+    word_vectors = []
+    for word in sentence:
+
+        if word in model.wv.index_to_key:
+            word_vectors.append(model.wv[word])
+            vector_length = len(model.wv[word])
+            if vector_length > max_vector_length:
+                max_vector_length = vector_length
+
     if word_vectors:
         avg_vector = np.mean(word_vectors, axis=0)
         document_vectors.append(avg_vector)
+    else:
+        document_vectors.append(None)
 
+
+print("document_vectors done")
 # Pad vectors to a fixed length
 document_vectors_padded = np.array([np.pad(vector, (0, max_vector_length - len(vector))) for vector in document_vectors])
 # Save for usage with other models later
